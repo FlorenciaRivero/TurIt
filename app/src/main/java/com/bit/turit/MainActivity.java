f92e9aad3d27f1886e5aa5942348e3cfe24d7f05
FlorenciaRivero <mflorenciarm1@gmail.com>
2021-03-11 00:27:56 -0300

    Se trabajó en fragments. Falta trabajar en comportamiento del botón home.

--- app/src/main/java/com/bit/turit/MainActivity.java
+++ app/src/main/java/com/bit/turit/MainActivity.java
@@ -1,9 +1,11 @@
 package com.bit.turit;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -11,14 +13,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     //Variables
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    ChipNavigationBar bottomNav;
+    FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
 
-        /*----------Tool Bar----------*/
+        /*----------Toolbar----------*/
         setSupportActionBar(toolbar);
 
         /*----------Navigation Drawer Menu----------*/
@@ -41,6 +49,54 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         navigationView.setNavigationItemSelectedListener(this);
 
+        /*----------Bottom Navigation Bar----------*/
+        bottomNav = findViewById(R.id.bottom_nav);
+
+        /*----------Para que se abra en el fragment home la aplicacion----------*/
+        /*if (savedInstanceState==null) {
+            bottomNav.setItemSelected(R.id.home, true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, homeFragment)
+                    .commit();
+        }*/
+
+
+        bottomNav.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int id) {
+                Fragment fragment = null;
+                switch (id){
+                    /*case R.id.home:
+                        fragment = new HomeFragment();
+                        break;*/
+                    case R.id.ticket:
+                        fragment = new TicketFragment();
+                        break;
+                    case R.id.agregar:
+                        fragment = new AgregarFragment();
+                        break;
+                    case R.id.lugares:
+                        fragment = new LugaresFragment();
+                        break;
+                    case R.id.tiendas:
+                        fragment = new TiendasFragment();
+                        break;
+
+                }
+
+                if (fragment!=null) {
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+                }else{
+                    Log.e(TAG, "Error in creating the fragment");
+                }
+            }
+
+        });
     }
 
     /*Para que no se cierre la app entera cuando abrimos el menu y apretamos botón para atrás.*/
